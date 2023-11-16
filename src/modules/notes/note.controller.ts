73a0375d8d39887b0d63416b4b9eaa63bbeb465d
bieudo1 <103914756+bieudo1@users.noreply.github.com>
2023-11-16 08:48:01 +0700

    feat Cache query data

--- src/modules/notes/note.controller.ts
+++ src/modules/notes/note.controller.ts
@@ -6,13 +6,19 @@ import {
   Delete,
   Body,
   Param,
+  Inject,
 } from '@nestjs/common';
+import { CACHE_MANAGER,CacheInterceptor,CacheTTL } from '@nestjs/cache-manager';
 import { NoteService } from './note.service';
+import { Cache } from 'cache-manager';
 import { Note } from './note.entity';
 
 @Controller('note')
 export class NoteController {
-  constructor(private readonly noteService: NoteService) {}
+  constructor(
+    private readonly noteService: NoteService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) { }
 
   //get all note
   @Get()
@@ -29,12 +35,25 @@ export class NoteController {
   //edit note by id
   @Put(':id')
   async update(@Param('id') id: number, @Body() note: Note): Promise<Note> {
+    await this.cacheManager.del(`demoCachekey`);
     return this.noteService.update(id, note);
   }
 
   //Delete note by id
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<void> {
+    await this.cacheManager.del(`demoCachekey`);
     return this.noteService.delete(id);
+  }
+  @Get('cache/demo/set-cache')
+  async democahe(): Promise<Note[]> {
+    const key = `demoCachekey`;
+    const cachedData = await this.cacheManager.get(key);
+    if (cachedData) {
+      return cachedData;
+    }
+    const data = await this.noteService.findAll();
+    await this.cacheManager.set(key, data, { ttl: 60*10 });
+    return data;
   }
 }
--- src/modules/notes/note.module.ts
+++ src/modules/notes/note.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { NoteController } from './note.controller';
+import { CacheModule } from '@nestjs/cache-manager';
 import { NoteService } from './note.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Note } from './note.entity';
-
+import * as redisStore from 'cache-manager-redis-store';
 @Module({
-  imports:[TypeOrmModule.forFeature([Note])],
+  imports: [
+  TypeOrmModule.forFeature([Note]),
+  CacheModule.register({
+    store: redisStore,
+    host: 'localhost',
+    port: 6379,
+  }),
+  ],
   controllers: [NoteController],
   providers: [NoteService],
 })
